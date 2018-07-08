cd65b8cc95dbfd7012955dc8a612adf4e639dd55
Kevin Dupas <dupas.kevin@gmail.com>
2018-07-08 15:39:49 +0200

    Premier blog en Symfony4

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class BlogController extends Controller
+{
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function index()
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        return $this->render('blog/home.html.twig');
+    }
+
+    /** 
+     * @Route("/blog/1", name="single")
+    */
+
+    public function single()
+    {
+        return $this->render('blog/single.html.twig');
+    }
+
+
+}
