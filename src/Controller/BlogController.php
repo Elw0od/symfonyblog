16d8d317cf3c9b1fdce1f1cc89b2f02a7d5c14d4
Kevin Dupas <dupas.kevin@gmail.com>
2018-07-09 20:05:49 +0200

    Ajout de la function create
    Ajout de la function create, utilisation de createFormBuilder ainsi que bootstrap_4_layout.html.twig et création d'un formulaire en utilisant le form de twig

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,6 +4,11 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
 
@@ -28,6 +33,36 @@ class BlogController extends Controller
     public function home()
     {
         return $this->render('blog/home.html.twig');
+    }
+
+    /**
+     * @Route("/blog/new", name="create_article")
+     */
+    public function create(Request $request)
+    {
+        $article = new Article();
+
+        $form = $this->createFormBuilder($article)
+                     ->add('title', TextType::class, [
+                         'attr' => [
+                             'label' => 'Titre',
+                             'placeholder' => "Titre de l'article"
+                         ]
+                     ])
+                     ->add('content', TextareaType::class, [
+                        'attr' => [
+                            'placeholder' => "Contenue de l'article"
+                        ]
+                    ])
+                     ->add('image', TextType::class, [
+                        'attr' => [
+                            'placeholder' => "Contenue de l'article"
+                        ]
+                     ])
+                     ->getForm();
+        return $this->render('blog/create.html.twig', [
+            'formArticle' => $form->createView()
+        ]);
     }
 
     /** 
